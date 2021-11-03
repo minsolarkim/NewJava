f9db3a8ebfde36c838d6fe15eadef0d680f3deb7
minsolarkim <77043336+minsolarkim@users.noreply.github.com>
2021-11-03 23:39:56 +0900

    Update Test4.java

--- Test4.java
+++ Test4.java
@@ -1,48 +1,31 @@
-//package jb01.test;
-//????????
-public class Test3 
+public class Test4 
 {
 	public static void main(String[] args) 
 	{
-		int num1 = Integer.parseInt(args[0]);
-		int num2 = Integer.parseInt(args[1]);
+		int score = Integer.parseInt(args[0]);
 
-		
-		if (num1 % 2 == 0)
+		if (score > 100 || score < 0)
 		{
-			System.out.print("입력하신 1번째 인자값 "+ num1 +"은 짝수이며 ");
-			if(num1 % 3 == 0) {
-				System.out.println("3의 배수입니다.");
-			} else {
-				System.out.println("3의 배수가 아닙니다.");
-			}
-			
+			System.out.print("잘못 입력하셨습니다. (0~100점 사이로 입력해주세요.)");
 		} else {
-			System.out.print("입력하신 1번째 인자값 "+ num1 +"은 홀수이며 ");
-			if( num1  % 3 == 0) {
-				System.out.println("3의 배수입니다.");
-			} else {
-				System.out.println("3의 배수가 아닙니다.");
-			}
-		}
-		
-		if (num2 % 2 == 0)
-		{
-			System.out.print("입력하신 2번째 인자값 "+ num2 +"은 짝수이며 ");
-			if(num2 % 3 == 0) {
-				System.out.println("3의 배수입니다.");
-			} else {
-				System.out.println("3의 배수가 아닙니다.");
-			}
-			
-		} else {
-			System.out.print("입력하신 2번째 인자값 "+ num2 +"은 홀수이며 ");
-			if( num2  % 3 == 0) {
-				System.out.println("3의 배수입니다.");
-			} else {
-				System.out.println("3의 배수가 아닙니다.");
-			}
-		}
+			System.out.print("입력하신 값 "+score+"점은 ");
 
+			if(score >= 90) {
+				System.out.print("수");
+			} else if (score >= 80)
+			{
+				System.out.print("우");
+			} else if (score >= 70)
+			{
+				System.out.print("미");
+			} else if (score >= 60)
+			{
+				System.out.print("양");
+			} else if (score < 60)
+			{
+				System.out.print("가");
+			} 
+			System.out.println("입니다.");
+		}
 	}
 }
