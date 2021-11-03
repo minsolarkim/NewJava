9559e728d87b2dcc449bd9ee9238c84a7111ced4
minsolarkim <77043336+minsolarkim@users.noreply.github.com>
2021-11-04 00:56:03 +0900

    home

---
+++ Example5.java
@@ -0,0 +1,15 @@
+class Example5 
+{
+	public static void main(String[] args) 
+	{
+		String[][] text = new String[9][9];
+
+		for (; ; )
+		{
+		}
+
+
+
+		System.out.println("Hello World!");
+	}
+}
