f361b0c27135f02c766822d1ea98111f9515a143
minsolarkim <77043336+minsolarkim@users.noreply.github.com>
2021-11-03 23:39:04 +0900

    Update Test4.java

--- Test4.java
+++ Test4.java
@@ -1,10 +1,48 @@
-public class Test4 
+//package jb01.test;
+//????????
+public class Test3 
 {
 	public static void main(String[] args) 
 	{
-		int score = Integer.parseInt(args[0]);
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
 
-		if(score >= 90) {
+		
+		if (num1 % 2 == 0)
+		{
+			System.out.print("입력하신 1번째 인자값 "+ num1 +"은 짝수이며 ");
+			if(num1 % 3 == 0) {
+				System.out.println("3의 배수입니다.");
+			} else {
+				System.out.println("3의 배수가 아닙니다.");
+			}
+			
+		} else {
+			System.out.print("입력하신 1번째 인자값 "+ num1 +"은 홀수이며 ");
+			if( num1  % 3 == 0) {
+				System.out.println("3의 배수입니다.");
+			} else {
+				System.out.println("3의 배수가 아닙니다.");
+			}
+		}
+		
+		if (num2 % 2 == 0)
+		{
+			System.out.print("입력하신 2번째 인자값 "+ num2 +"은 짝수이며 ");
+			if(num2 % 3 == 0) {
+				System.out.println("3의 배수입니다.");
+			} else {
+				System.out.println("3의 배수가 아닙니다.");
+			}
+			
+		} else {
+			System.out.print("입력하신 2번째 인자값 "+ num2 +"은 홀수이며 ");
+			if( num2  % 3 == 0) {
+				System.out.println("3의 배수입니다.");
+			} else {
+				System.out.println("3의 배수가 아닙니다.");
+			}
+		}
 
 	}
 }
